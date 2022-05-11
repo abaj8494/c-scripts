71599f2dce2930c49ed48a41bc664f543cfcd8ce
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-05-11 10:45:18 +1000

    moved directory

--- quadratic.c
+++ quadratic.c
@@ -8,11 +8,11 @@ int main(int argc, char *argv[]) {
 	int c = atoi(argv[3]);
 	double disc = b*b - 4*a*c;
 	if (disc < 0) {
-printf("No real roots.\n");		
+printf("<items><item><title>No real roots.</title><subtitle></subtitle><icon>icon.png</icon></item></items>");		
 		return 0;
 	}
 	double root1 = (-b + sqrt(disc))/(2*a);
 	double root2 = (-b - sqrt(disc))/(2*a);	
-	printf("Roots at %.4lf and %.4lf\n", root1, root2);
+	printf("<items><item><title>Roots at \"%.4lf\" and \"%.4lf\"</title><icon>icon.png</icon></item></items>", root1, root2);
 	return 0;
 }
---
+++ temp.c
@@ -0,0 +1,4 @@
+    // for (raised = base; user_num % raised < user_num; i++ ) {
+    //     raised = pow(base, i);
+    //     base_cap = i - 1;
+    // }
