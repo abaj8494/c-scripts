e8d69d221ac4b749c1acb3e696e59eac3cf73aab
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-09-24 19:50:05 +1000

    crontab 19:50:05

--- gcd.c
+++ gcd.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 
 int q_func(int a, int b) {
-    return a % b;
+    return a / b;
 }
 
 int r_func(int a, int b, int q) {
@@ -23,6 +23,8 @@ int main(int argc, char *argv[]) {
     int a = atoi(argv[1]), b = atoi(argv[2]), 
         q = q_func(a, b), r = r_func(a, b, q);
 
+    printf("values: a%d, b%d, q%d, r%d\n", a,b,q,r);
+    return 0;
     while (r) {
         a = b;
         b = r;
