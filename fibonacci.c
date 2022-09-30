f942708bdb14a6dc33189e4fa8ab76e577371b43
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-01 09:37:04 +1000

    crontab 09:37:04

--- fibonacci.c
+++ fibonacci.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int fib(int f) {
     return f;
 }
 
 
-int main(int argc, char argv**) {
+int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <int>\n", argv[0]);
         return 1;
     }
 
-    return fib(atoi(argv[1]));
+    printf("%d\n", fib(atoi(argv[1])));
+    return 0;
 }
