6cab28f74e98f1ec12f499d303522c0371886498
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-01 09:55:05 +1000

    crontab 09:55:05

--- mergesort.c
+++ mergesort.c
@@ -4,10 +4,18 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
+    // error checking
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <int> <int> ... <int>\n", argv[0]);
         return 1;
     }
-    printf("argc: %d", argc);
+    // copy into int array
+    int to_sort[argc - 1];
+    for (int i = 0, j = 1; i < argc - 1; i++, j++) {
+        to_sort[i] = atoi(argv[j]);
+        printf("to_sort[i] = %d\n", to_sort[i]);
+    }
+
+
     return 0;
 }
