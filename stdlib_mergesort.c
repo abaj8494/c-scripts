bdb6f4b3dc54122f900f98f08f0eaeb7a06b13fa
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-02 09:19:06 +1100

    crontab 09:19:06

--- stdlib_mergesort.c
+++ stdlib_mergesort.c
@@ -1,1 +1,15 @@
+// running mergesort provided by stdlib
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define key(A) (A)
+#define less(A, B) (key(A) < key(B))
+
+int main(void) {
+    int a[] = {11, 6, 10, 7, 5, 1, 3, 2, 4, 8, 9};
+
+    mergesort(a, 11, 4, less);
+
+    for (int i = 0; i < 11; i++) printf("%d\n", a[i]);
+}
