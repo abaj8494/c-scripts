e3c4e5dce04aff8b43c4d08d90fe5ee8dc22ece9
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-01 10:53:05 +1000

    crontab 10:53:05

--- mergesort.c
+++ mergesort.c
@@ -3,27 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *merge(int *a, int l, int m, int r) {
+int *b;
+
+void merge(int *a, int l, int m, int r) {
     int i, j, k;
-    int *b = malloc(sizeof(int) * r);
     for (int i = m + 1; i > l; i--) b[i - 1] = a[i - 1];
     for (int j = m; j < r; j++) b[r + m - j] = a[j + 1];
     for (int k = l; k <= r; k++) {
         if (b[j] < b[i]) a[k] = b[j--];
         else a[k] = b[i++];
     }
-    return b;
 }
 
-int *msort(int *a, int l, int r) {
+void msort(int *a, int l, int r) {
     // l is the left index, m is the middle, r is the right.
     int m = (l + r) / 2;
     if (l < r) {
         msort(a, l, m);
         msort(a, m + 1, r);
-        return merge(a, l, m, r);
+        merge(a, l, m, r);
     }
-    return NULL;
 }
 
 int main(int argc, char **argv) {
@@ -38,7 +37,8 @@ int main(int argc, char **argv) {
         a[i] = atoi(argv[j]);
     }
 
-    int *b = msort(a, 0, argc - 1);
+    b = malloc(sizeof(int) * r);
+    msort(a, 0, argc - 1);
     for (int i = 0; i < argc - 1; i++) {
         printf("%d\n", b[i]);
     }
