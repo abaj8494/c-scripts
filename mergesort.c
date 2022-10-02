816de3aaec6bd6fd85eafb21266ef6757abeeff3
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-02 17:51:05 +1100

    crontab 17:51:05

--- mergesort.c
+++ mergesort.c
@@ -4,27 +4,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define key(A) (A)
-#define less(A, B) (key(A) < key(B))
-
-int aux[8];
-
 void merge(int a[], int l, int m, int r)
 {
-    printf("r:%d\n",r);
-  int i, j, k;
-
-  for (i = m+1; i > l; i--)
-    aux[i-1] = a[i-1];
-  for (j = m; j < r; j++)
-    aux[r+m-j] = a[j+1];
-  for (k = l; k <= r; k++)
-    if (less(aux[j], aux[i]))
-      a[k] = aux[j--];
-    else
-      a[k] = aux[i++];
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    /* create temp arrays */
+    int L[n1], R[n2];
+ 
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
-
 
 void msort(int *a, int l, int r) {
     // l is the left index, m is the middle, r is the right.
@@ -51,7 +77,7 @@ int main(int argc, char **argv) {
     msort(a, 0, argc - 2);
 
     for (int i = 0; i < argc - 1; i++) {
-        printf("%d\n", aux[i]);
+        printf("%d\n", a[i]);
     }
     
     return 0;
