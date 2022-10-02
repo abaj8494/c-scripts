662b5c63625c8e5933a8153054833c18a38bc23a
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-03 09:32:06 +1100

    crontab 09:32:06

--- mod-congruence.c
+++ mod-congruence.c
@@ -1,1 +1,9 @@
-// solves 
+// solves the integer values which satisfy the equation ax $\congruent$ c (mod b)
+// uses the euclidean algorithm
+
+#include <stdio.h>
+#include "gcd.h"
+
+int main(int argc, char **argv) {
+    
+}
