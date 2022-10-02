74322e1eb162fe8799e11477af825d777dff1d31
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-02 15:36:05 +1100

    crontab 15:36:05

--- merge2arrays.c
+++ merge2arrays.c
@@ -5,11 +5,11 @@
 #include <stdlib.h>
 
 
-int *merge(int *a, int *b) {
-    int *c = malloc(32);
+int *merge(int *a, int *b, int n) {
+    int *c = malloc(sizeof(int) * n);
 
     int i = 0, j = 0, k = 0;
-    while (i < 8) {
+    while (i < 2*n) {
         if (a[i] < b[j]) {c[k++] = a[i++];}
         else if (b[j] < a[i]) {c[k++] = b[j++];}
         else {c[k++] = a[i++]; c[k++] = b[j++];}
@@ -20,9 +20,10 @@ int *merge(int *a, int *b) {
 }
 
 int main(void) {
-    int a[] = {2, 3, 11, 19};
-    int b[] = {4, 9, 16, 25};
+    int a[] = {1, 2, 3, 11, 16, 17, 19, 22, 29, 79};
+    int b[] = {4, 9, 16, 25, 41, 45, 49, 66, 78, 79};
 
-    int *c = merge(a, b);
-    for (int i = 0; i < 8; i++) {printf("%d\n", c[i]);}
+
+    int *c = merge(a, b, 10);
+    for (int i = 0; i < 20; i++) {printf("%d\n", c[i]);}
 }
