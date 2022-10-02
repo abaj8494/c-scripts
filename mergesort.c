d4e95236b65636599db16179d97e861a3a0650e2
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-02 17:52:05 +1100

    crontab 17:52:05

--- mergesort.c
+++ mergesort.c
@@ -1,4 +1,4 @@
-// sorts an array of integers taken as space delimited arguments
+// sorts an aay of integers taken as space delimited arguments
 // [[October  1st, 2022]] not working...
 
 #include <stdio.h>
@@ -10,26 +10,26 @@ void merge(int a[], int l, int m, int r)
     int n1 = m - l + 1;
     int n2 = r - m;
  
-    /* create temp arrays */
+    /* create temp aays */
     int L[n1], R[n2];
  
-    /* Copy data to temp arrays L[] and R[] */
+    /* Copy data to temp aays L[] and R[] */
     for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
+        L[i] = a[l + i];
     for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
+        R[j] = a[m + 1 + j];
  
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
+    /* Merge the temp aays back into a[l..r]*/
+    i = 0; // Initial index of first subaay
+    j = 0; // Initial index of second subaay
+    k = l; // Initial index of merged subaay
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
-            arr[k] = L[i];
+            a[k] = L[i];
             i++;
         }
         else {
-            arr[k] = R[j];
+            a[k] = R[j];
             j++;
         }
         k++;
@@ -38,7 +38,7 @@ void merge(int a[], int l, int m, int r)
     /* Copy the remaining elements of L[], if there
     are any */
     while (i < n1) {
-        arr[k] = L[i];
+        a[k] = L[i];
         i++;
         k++;
     }
@@ -46,7 +46,7 @@ void merge(int a[], int l, int m, int r)
     /* Copy the remaining elements of R[], if there
     are any */
     while (j < n2) {
-        arr[k] = R[j];
+        a[k] = R[j];
         j++;
         k++;
     }
@@ -68,7 +68,7 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Usage: %s <int> <int> ... <int>\n", argv[0]);
         return 1;
     }
-    // copy into int array
+    // copy into int aay
     int a[argc - 1];
     for (int i = 0, j = 1; i < argc - 1; i++, j++) {
         a[i] = atoi(argv[j]);
