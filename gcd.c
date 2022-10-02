23857744ee8d02aea347e5bd69453012eb5f22ae
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-03 10:56:00 +1100

    crontab 10:56:00

--- gcd.c
+++ gcd.c
@@ -3,7 +3,30 @@
 #include <stdio.h>
 #include "gcd.h"
 
+/* this is the simple algorithm
 int gcd(int a, int b) {
     if (!b) return a;
     return gcd(b, a % b);
+}
+*/
+
+int gcd(int a, int b, int *x, int *y)
+{
+    // Base Case
+    if (a == 0)
+    {
+        *x = 0;
+        *y = 1;
+        return b;
+    }
+ 
+    int x1, y1; // To store results of recursive call
+    int gcd = gcd(b%a, a, &x1, &y1);
+ 
+    // Update x and y using results of recursive
+    // call
+    *x = y1 - (b/a) * x1;
+    *y = x1;
+ 
+    return gcd;
 }
