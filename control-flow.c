e0ce6798c577dedd841f758893f7c61424177d85
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-03 10:32:01 +1100

    crontab 10:32:01

---
+++ control-flow.c
@@ -0,0 +1,11 @@
+// understanding basic control flow
+
+#include <stdio.h>
+
+int main(void) {
+    if (0) printf("if 0\n");
+    if (1) printf("if 1\n");
+    if (2) printf("if 2\n");
+    while (0) {printf("while 0\n");}
+    return 0;
+}
