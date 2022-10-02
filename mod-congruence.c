785a446f03318f9683724c612fd051ed157dbd1d
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-03 09:38:05 +1100

    crontab 09:38:05

--- mod-congruence.c
+++ mod-congruence.c
@@ -2,11 +2,17 @@
 // uses the euclidean algorithm
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "gcd.h"
 
 int main(int argc, char **argv) {
     if (argc != 4) {
+        fprintf(stderr, "Usage: %s a b c\n", argv[0]);
+        return  1;
+    }
+    int a = atoi(argv[1]), b = atoi(argv[2], c = atoi(argv[3])); 
+    int d = gcd(a, b);
 
-        }
+    printf("gcd is %d\n", d);
     
 }
