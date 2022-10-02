794e0254f93538da75d2ba41820591555b187260
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-03 10:07:06 +1100

    crontab 10:07:06

--- gcd.c
+++ gcd.c
@@ -6,7 +6,7 @@
 int gcd(int a, int b) {
     int r = a % b;
     int q = (a - r) / b;
-    printf("a r b %d %d %d\n",a, r, b);
+    printf("a b r %d %d %d\n",a, b, r);
     if (r) gcd(b, r);
-    return b;
+    return 3;
 }
