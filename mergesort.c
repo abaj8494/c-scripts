54892fab2982e2d2ce799a7ae5219cf33203d618
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-02 17:16:05 +1100

    crontab 17:16:05

--- mergesort.c
+++ mergesort.c
@@ -17,6 +17,7 @@ void merge(int *a, int l, int m, int r) {
             // if about to exit, copy the rest of the array into b
             if (i == m + 1) {
                 printf("in loop\n");
+                for (int n = j; n < r; n++) { b[k - j + n] = a[n]; }
             }
         }
         else {
@@ -25,6 +26,7 @@ void merge(int *a, int l, int m, int r) {
             // if about to exit, copy the rest of the array into b
             if (j == r + 1) {
                 printf("in loop\n");
+                for (int n = i; n < m; n++) { b[k - i + n] = a[n]; }
             }
 
         }
