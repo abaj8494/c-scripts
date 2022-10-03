db742034ba1160ddd3634ad816a4cc35cb9d5933
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-03 11:28:01 +1100

    crontab 11:28:01

--- mod-congruence.c
+++ mod-congruence.c
@@ -1,4 +1,4 @@
-// solves the integer values which satisfy the equation ax $\congruent$ c (mod b)
+// solves the integer values which satisfy the equation ax equiv b (mod m)
 // uses the euclidean algorithm
 
 #include <stdio.h>
@@ -11,16 +11,16 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Usage: %s a b c\n", argv[0]);
         return  1;
     }
-    int a = atoi(argv[1]), b = atoi(argv[3]), c = atoi(argv[2]), x, y; 
-    int d = gcd(a, b, &x, &y);
+    int a = atoi(argv[1]), b = atoi(argv[2]), m = atoi(argv[3]), x, y; 
+    int d = gcd(a, m, &x, &y);
 
     // ax equiv b (mod) c => ax + by = c
     if (!(d % b)) printf("no solutions\n");
     else {
         printf("Solutions are: ");
         for (int i = 0; i < d; i++) {
-            if (x < 0) x = mod(x,c);
-            printf("%d ", (x + i * c/d) % c);
+            if (x < 0) x = mod(x,m);
+            printf("%d ", (x + i * m/d) % m);
         }
         printf("\n");
     }
