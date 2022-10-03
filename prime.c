738710b76aa2039811073587eb2598464fd2b83d
Aayush Bajaj <aayushbajaj7@gmail.com>
2022-10-03 14:23:00 +1100

    crontab 14:23:00

--- prime.c
+++ prime.c
@@ -1,18 +1,12 @@
+// checks if an integer is prime
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main(int argc, char *argv[]) {
-	int factors = 0, total_primes = 0;
-	for (int i = 1; i <= atoi(argv[1]); i++) {
-		factors = 0;
-		for (int j = 1; j <= i; j++) {
-			if (i % j == 0) factors++;
-		}
-		if (factors == 2) {
-			total_primes++;
-			printf("%d\n", i);
-		}
-	}
-	printf("Total Primes: %d\n", total_primes);
-	return 0;
+int main (int argc, char **argv) {
+    int p = atoi(argv[1]);
+    int max = (int) sqrt(p);
+    printf("p max : %d %d", p, max);
+    return 0;
 }
